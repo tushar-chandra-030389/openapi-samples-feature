a1479ce7fc5135ae4e6bb525182751ce71404722
Vinay Gosain (VING) <ving@saxobank.com>
2017-09-22 10:15:38 +0530

    eslint file name change

---
+++ src/js/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import loaderReducer from './modules/loader/reducer';
+import errorReducer from './modules/error/reducer';
+import userInfoReducer from './modules/userInfo/reducer';
+import AppRoute from './modules/app';
+import '../css/style.css';
+
+//css
+import '../css/style.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const reducer = combineReducers({
+    loader: loaderReducer,
+    error: errorReducer,
+    userInfo: userInfoReducer,
+});
+const store = createStore(reducer, applyMiddleware(thunk));
+
+render(
+    <Provider store={store}>
+        <AppRoute />
+    </Provider>,
+    document.getElementById('container')
+);
