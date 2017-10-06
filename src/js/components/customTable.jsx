25ebf2a61ab9a527659dcfa480219b27ad980fa9
Abhishek Patel <abpa@saxobank.com>
2017-10-06 12:05:31 +0530

    adding group functionality in info prices module and data retention functionality in prices module

--- src/js/components/customTable.jsx
+++ src/js/components/customTable.jsx
@@ -10,16 +10,16 @@ class CustomTable extends React.PureComponent {
     constructor(props) {
         super(props);
         this.data = [];
-        this.handleData(props);
+        this.handleData();
     }
 
     componentWillReceiveProps(newProps) {
         this.handleData(newProps);
     }
 
-    handleData(props) {
+    handleData() {
         this.data = [];
-        _.forEach(props.data, (obj) => {
+        _.forEach(this.props.data, (obj) => {
             this.data.push(obj);
         });
     }
